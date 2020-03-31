fdafe3bc7b945f21f8396236a6e47bb62053aace
moacirtorres <moacirtorress@gmail.com>
2020-03-31 04:55:39 -0300

    Atualizing words and caracters count while digiting in the main input

--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,14 @@ let numeroPalavras = frase.split(" ").length
 
 let tamanhoFrase = $("#tamanho-frase")
 
-tamanhoFrase.text(numeroPalavras) 
+tamanhoFrase.text(numeroPalavras)
+
+var campo = $(".campo-digitacao")
+
+campo.on("input", function() {
+	let conteudo = campo.val()
+	let qtdPalavras = conteudo.split(/\S+/).length - 1
+	$("#contador-palavras").text(qtdPalavras)
+	$("#contador-caracteres").text(conteudo.length)
+
+})
