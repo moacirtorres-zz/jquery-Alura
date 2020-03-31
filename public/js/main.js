9c6b60b4ec746ac65036bc253a42aeba9dcd32dd
moacirtorres <moacirtorress@gmail.com>
2020-03-31 19:11:14 -0300

    Setting timer to your game

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,8 @@ tamanhoFrase.text(numeroPalavras)
 
 var campo = $(".campo-digitacao")
 
+// Contador de caracteres e palavras
+
 campo.on("input", function() {
 	let conteudo = campo.val()
 	let qtdPalavras = conteudo.split(/\S+/).length - 1
@@ -15,3 +17,24 @@ campo.on("input", function() {
 	$("#contador-caracteres").text(conteudo.length)
 
 })
+
+//Implementando gameover no código
+
+
+var tempoRestante = $("#tempo-digitacao").text()
+campo.one("focus", function() {
+
+	var cronometroID = setInterval(function(){
+
+		tempoRestante--
+		$("#tempo-digitacao").text(tempoRestante)
+		if (tempoRestante < 1) {
+			campo.attr("disabled", true)
+			clearInterval(cronometroID)
+		}
+	}, 1000)	
+
+})
+
+
+
